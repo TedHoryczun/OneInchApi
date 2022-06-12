f667772329faad2d498f760e4e5d6f0e55a29535
TedHoryczun <tedhoryczuniv@outlook.com>
2022-06-12 12:29:20 -0400

    update chain_id

--- one_inch/__init__.py
+++ one_inch/__init__.py
@@ -4,24 +4,26 @@ import requests
 class OneInchApi:
     base_url = 'https://api.1inch.io/v4.0/'
     wallet: str
+    chain_id: str
 
-    def __init__(self, wallet: str):
+    def __init__(self, wallet: str, chain_id: str):
         self.wallet = wallet
+        self.chain_id = chain_id
 
-    def liquidity_source(self, chain_id: str):
-        url = 'https://api.1inch.io/v4.0/' + chain_id + '/liquidity-sources'
+    def liquidity_source(self):
+        url = 'https://api.1inch.io/v4.0/' + self.chain_id + '/liquidity-sources'
         return requests.get(url)
 
-    def quote(self, from_token: str, to_token: str, amount: str, chain_id: str, protocol_ids: str = ''):
-        url = self.base_url + chain_id + "/quote?fromTokenAddress=" + from_token + \
+    def quote(self, from_token: str, to_token: str, amount: str, protocol_ids: str = ''):
+        url = self.base_url + self.chain_id + "/quote?fromTokenAddress=" + from_token + \
               "&toTokenAddress=" + to_token + \
               "&amount=" + amount
         if protocol_ids != '':
             url += '&protocols=' + protocol_ids
         return requests.get(url)
 
-    def do_swap(self, chain_id, from_token_addr, to_token_addr, amount, slippage, protocol_ids: str) -> any:
-        url = self.base_url + chain_id + '/swap?fromTokenAddress=' + from_token_addr + \
+    def do_swap(self, from_token_addr, to_token_addr, amount, slippage, protocol_ids: str) -> any:
+        url = self.base_url + self.chain_id + '/swap?fromTokenAddress=' + from_token_addr + \
               '&toTokenAddress=' + to_token_addr + \
               '&amount=' + amount + '&fromAddress=' + self.wallet + \
               '&slippage=' + slippage + \
@@ -30,12 +32,12 @@ class OneInchApi:
             url += '&protocols=' + protocol_ids
         return requests.get(url)
 
-    def check_allowance(self, token, chain_id):
-        url = self.base_url + chain_id + '/approve/allowance?tokenAddress=' + token + \
+    def check_allowance(self, token):
+        url = self.base_url + self.chain_id + '/approve/allowance?tokenAddress=' + token + \
               '&walletAddress=' + self.wallet
         return requests.get(url)
 
-    def approve_tx(self, token, amount, chain_id):
-        url = self.base_url + chain_id + '/approve/transaction?tokenAddress=' + token + \
+    def approve_tx(self, token, amount):
+        url = self.base_url + self.chain_id + '/approve/transaction?tokenAddress=' + token + \
               '&amount=' + amount
         return requests.get(url)
