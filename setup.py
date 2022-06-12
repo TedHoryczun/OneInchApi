e781ea5e9b27abb11eac7bd294013e3d4f969309
TedHoryczun <tedhoryczuniv@outlook.com>
2022-06-12 12:11:49 -0400

    update setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='OneInch',
+    version='0.1',
+    packages=['requests'],
+    url='',
+    license='',
+    author='tedhoryczun',
+    author_email='',
+    description='OneInch Api',
+)
